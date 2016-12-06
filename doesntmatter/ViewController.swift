0bcfa3598e6441d296c7707eefad2d66d01beefb
Knuckles Zucchini <mkrawczuk@tw-mbp-mkrawczuk.attlocal.net>
2016-12-06 11:55:22 -0800

    First commit of class code.

--- doesntmatter/ViewController.swift
+++ doesntmatter/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount > 10 {
+            theLabel.text = "You tapped the button more than 10 times."
+        }
+        
+        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
